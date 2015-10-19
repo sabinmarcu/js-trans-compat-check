b3f2b61edae91cd1c02e0b668ad721d7f4dd252d
Sabin Marcu <sabinmarcu@gmail.com>
2015-10-19 15:27:13 +0100

    added port to express server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var Express = require("express"),
     app = Express();
 
+app.set('port', (process.env.PORT || 3000));
+
 app.use(Express.static("public"));
 
-var server = app.listen(3000, function () {
+var server = app.listen(app.get("port"), function () {
   var host = server.address().address;
   var port = server.address().port;
 
