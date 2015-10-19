15ea90d3ce452761b4c9b8119b963773d415fcea
Sabin Marcu <sabinmarcu@gmail.com>
2015-10-19 15:17:00 +0100

    Demo server and deploy travis

---
+++ server.js
@@ -0,0 +1,11 @@
+var Express = require("express"),
+    app = Express();
+
+app.use(Express.static("public"));
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
